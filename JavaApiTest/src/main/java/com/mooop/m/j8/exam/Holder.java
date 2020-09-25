51f918259cf3c8ca8faca98b3de7e361bfbadb9a
[지나웍스] 김철우 <cwkim@zinnaworks.com>
2020-09-25 19:30:59 +0900

    20200925_02

--- JavaApiTest/src/main/java/com/mooop/m/j8/exam/Holder.java
+++ JavaApiTest/src/main/java/com/mooop/m/j8/exam/Holder.java
@@ -2,6 +2,14 @@ package com.mooop.m.j8.exam;
 
 import java.util.function.Supplier;
 
+/**
+ * 
+ * Lazy 호출및 가시성 문제 해결 ( thread safe )
+ *  -  lambda를 이용하는 방법이 핵심기술임..
+ * 
+ * @author MOoop
+ *
+ */
 public class Holder {
 	
 	private Supplier<Heavy> heavy = ()->createAndCacheHeavy();
