a966348585f60482bb9d8a739db5b32dd48c5c2b
[지나웍스] 김철우 <cwkim@zinnaworks.com>
2021-09-30 19:35:46 +0900

    ing

---
+++ JavaApiTest/src/main/java/com/mooop/m/designpattern/movie2/DiscountConditionType.java
@@ -0,0 +1,6 @@
+package com.mooop.m.designpattern.movie2;
+
+public enum DiscountConditionType {
+    SEQUENCE,       //순번 조건
+    PERIOD          //기간 조건
+}
---
+++ JavaApiTest/src/main/java/com/mooop/m/designpattern/movie2/MoiveType.java
@@ -0,0 +1,7 @@
+package com.mooop.m.designpattern.movie2;
+
+public enum MoiveType {
+    AMOUNT_DISCOUNT,    //금액 할인 정책
+    PERCENT_DISCOUNT,   //비율 할인 정책
+    NONE_DISCOUNT       //미적용
+}
